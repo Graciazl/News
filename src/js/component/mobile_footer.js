67686713e08b88cbcd893f0af2ab779643bc255d
Gracia <gracia@devlevelup.com>
2017-09-19 11:15:13 -0400

    Footer

---
+++ src/js/component/mobile_footer.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Gracia on 17/9/19.
+ */
+import React from 'react';
+import {Row, Col} from 'antd';
+
+export default class MobileFooter extends React.Component {
+
+    render() {
+        return (
+            <footer>
+                <Row>
+                    <Col span={2}>
+
+                    </Col>
+                    <Col span={20} class="foter">
+                        &copy;&nbsp;2017 News. All Rights Reserved.
+                    </Col>
+                    <Col span={2}>
+
+                    </Col>
+                </Row>
+            </footer>
+        );
+    }
+}
--- src/js/component/pc_index.js
+++ src/js/component/pc_index.js
@@ -3,11 +3,15 @@
  */
 import React from 'react';
 import PCHeader from './pc_header';
+import PCFooter from './pc_footer';
 
 export default class PCIndex extends React.Component {
     render() {
         return (
-            <PCHeader/>
+            <div>
+                <PCHeader/>
+                <PCFooter/>
+            </div>
         );
     }
 }
