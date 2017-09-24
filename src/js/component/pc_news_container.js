8d7d64f6d0a8eddb7f9963f999d9e2f0a31a9829
Gracia <gracia@devlevelup.com>
2017-09-24 11:57:40 -0400

    Carousel picture.

---
+++ src/js/component/pc_news_container.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Gracia on 17/9/24.
+ */
+import React from 'react';
+import {Row, Col, Tabs, Carousel} from 'antd';
+
+const TabPane = Tabs.TabPane;
+
+export default class pcNewsContainer extends React.Component {
+    render() {
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            autoplay: true
+        };
+
+        return (
+          <Row>
+              <Col span={2}></Col>
+              <Col span={20} class="container">
+                  <div class="leftContainer">
+                      <div className="carousel">
+                          <Carousel {...settings}>
+                              <div><img src="./src/images/carousal_1.jpg"/></div>
+                              <div><img src="./src/images/carousal_2.jpg"/></div>
+                              <div><img src="./src/images/carousal_3.jpg"/></div>
+                              <div><img src="./src/images/carousal_4.jpg"/></div>
+                          </Carousel>
+                      </div>
+                  </div>
+              </Col>
+              <Col span={2}></Col>
+          </Row>
+        );
+    }
+}
