f7b0209602343f1696357416ad265b1265a8b7b5
Gracia <gracia@devlevelup.com>
2017-09-27 20:50:42 -0400

    no message

---
+++ src/js/component/pc_details.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class PCNewsDetails extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            newsItem: ''
+        };
+    }
+
+
+}
